132b601d95f94d3d03ca306bb872972615034be8
Amir Rahi <amirrahi29@gmail.com>
2023-03-18 14:10:34 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import ToDoUncontrolledComponent from './ToDoUncontrolledComponent'
+import Profile from './Profile'
 
 const App = () => {
   return (
-    // <View>
-    //   <Profile myData = {'amir'} status='it' newData = {38}  />
-    //   <Profile myData = {'amir'} status='coder' newData = {38} anyData = 'Helooooooooooo' />
+    <>
+    <View>
+      <Profile myData = {'amir'} status='it' newData = {38}  />
+      <Profile myData = {'amir'} status='coder' newData = {38} anyData = 'Helooooooooooo' />
 
-    //   <Profile myData = {'amir'} status='it' newData = {38}>
-    //     passing text directly
-    //   </Profile>
+      <Profile myData = {'amir'} status='it' newData = {38}>
+        passing text directly
+      </Profile>
       
-    // </View>
+    </View>
     <ToDoUncontrolledComponent/>
+    </>
   )
 }
 
